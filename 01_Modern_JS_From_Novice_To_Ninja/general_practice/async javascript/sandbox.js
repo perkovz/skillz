15866755b5437398bfb71c24051b6ff765d69748
Zvonimir Perkovic <perkovz@ccf.org>
2020-08-24 14:46:09 -0400

    XMLHttpRequest, Fetch API, asinc and await.

--- 01_Modern_JS_From_Novice_To_Ninja/general_practice/async javascript/sandbox.js
+++ 01_Modern_JS_From_Novice_To_Ninja/general_practice/async javascript/sandbox.js
@@ -1,14 +1,60 @@
-const request = new XMLHttpRequest();
+// Asinc and Await
+const getTodos = async () => {
+    const response = await fetch('mario.json');
 
-request.addEventListener('readystatechange', () => {
-    //console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request);
+    if(response.status !== 200) {
+        throw new Error('Cannot fetch the data.');
     }
-    else if(request.readyState === 4) {
-        console.log('could not fetch the data');
-    }
+
+    const data = await response.json();
+    return data;
+};
+
+getTodos()
+    .then(data => console.log('resolved: ', data))
+    .catch(err => console.log('rejected: ', err.message));
+
+
+// Fetch API
+/*
+fetch('mario.json').then((response) => {
+    console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    console.log(data);
+}).catch((err) => {
+    console.log('rejected', err);
 });
+*/
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-request.send();
+
+// XMLHttpRequest Object
+/*
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', () => {
+            if(request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            }
+            else if(request.readyState === 4) {
+                reject('Could not fetch data.');
+            }
+        });
+        
+        request.open('GET', resource);
+        request.send();
+    });
+};
+
+getTodos('mario.json').then(data => {
+    console.log('promise 1 resolved: ', data);
+    return getTodos('shaun.json');
+}).then(data => {
+    console.log('promise 2 resolved: ', data);
+}).catch(err => {
+    console.log('promise rejected', err);
+});
+*/
