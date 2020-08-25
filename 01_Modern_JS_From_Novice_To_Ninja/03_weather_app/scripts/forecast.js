cfc203411aacad988404010392417cb3fedb3afd
Zvonimir Perkovic <perkovz@ccf.org>
2020-08-25 15:56:53 -0400

    Getting city and weather from AccuWeather API

---
+++ 01_Modern_JS_From_Novice_To_Ninja/03_weather_app/scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = "OZNzoMHp8dbsPoBpbP6jg1KahFm0UpiE";
+ 
+const getWeather = async(id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+const getCity = async(city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+/*
+getCity('osijek').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
+*/
