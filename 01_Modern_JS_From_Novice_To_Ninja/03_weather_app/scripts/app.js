2347df1c1db3f232d8d366f7be18b2f614cf7cd6
Zvonimir Perkovic <perkovz@ccf.org>
2020-08-28 08:26:37 -0400

    Fixed github alerts.

--- 01_Modern_JS_From_Novice_To_Ninja/03_weather_app/scripts/app.js
+++ 01_Modern_JS_From_Novice_To_Ninja/03_weather_app/scripts/app.js
@@ -1,20 +1,19 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
-const updateCity = async(city) => {
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
+const updateCity = async (city) => {
+	const cityDetails = await getCity(city);
+	const weather = await getWeather(cityDetails.Key);
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+	return { cityDetails, weather };
 };
 
 cityForm.addEventListener('submit', e => {
-    e.preventDefault();
+	e.preventDefault();
 
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
-    
-    updateCity(city).then(data => console.log(data)).catch(err => console.log(err));
+	const city = cityForm.city.value.trim();
+	cityForm.reset();
+
+	updateCity(city).then(data => console.log(data)).catch(err => console.log(err));
 });
