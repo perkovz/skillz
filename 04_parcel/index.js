2657947c4380b8052cb1c332d3005ba05427d658
Zvonimir Perkovic <perkovz@ccf.org>
2021-01-12 13:52:56 -0500

    parcel

---
+++ 04_parcel/index.js
@@ -0,0 +1,6 @@
+import './styles/tokens.scss';
+import './styles/nav.scss';
+import './styles/panel.scss';
+
+import './scripts/nav.js';
+import './scripts/panel.js';
---
+++ 04_parcel/scripts/panel.js
@@ -0,0 +1,20 @@
+// JS 2016
+
+let x = 5;
+let z = x ** 2; 
+
+let y = 5;
+y **= 2;
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(z);
+console.log(y);
+console.log(fruits.includes("Mango"));
+
+// JS 2017
+
+let str = "5";
+str = str.padStart(4,0);
+
+console.log(str);
